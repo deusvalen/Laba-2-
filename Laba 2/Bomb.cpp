db925153c7cd8e635c77f20a8a970cf79c3dba23
Валентин Лёля < >
2016-09-08 21:53:12 +0200

    Almost done! Tabulation and a little bit stylish code

--- Laba 2/Bomb.cpp
+++ Laba 2/Bomb.cpp
@@ -4,213 +4,204 @@
 using namespace std;
 
 
-Student::Student()
-{
-}
-
-
-Student::Student(char* NAME, int AGE)
-{
- age = AGE;
- strcpy(name, NAME);
-}
-
-
-void Student::setName()
-{
- cout << "enter name : " << endl;
- cin >> name;
-}
-void Student::setGroup()
-{
- cout << "enter group : " << endl;
- cin >> group;
- if (group <= 0)	
+	Student::Student()
 	{
-	 cout << "Wrong group, type it again" << endl;
-	 setGroup();
 	}
-}
-void Student::setSurname()
-{
- cout << "enter surname : " << endl;
- cin >> surname;
-}
-void Student::setPatronymic()
-{
- cout << "enter patronymic : " << endl;
- cin >> patronymic;
-}
-void Student::setId()
-{
- cout << "enter id : " << endl;
- cin >> id;
- while (id < 0 || cin.fail()  )	
+
+	Student::Student(char* NAME, int AGE)
 	{
-	  cin.clear();
-      cin.sync();
-	 cout << "Wrong id, type it again" << endl;
-	 setId();
+		 age = AGE;
+		 strcpy(name, NAME);
 	}
-}
-void Student::setFaculty()
-{
- cout << "enter faculty : " << endl;
- cin >> faculty;
-}
-void Student::setAdress()
-{
- cout << "enter adress : " << endl;
- cin >> adress;
-}
-void Student::setNumber()
-{
- cout << "enter number : " << endl;
- cin >> number;
- while (age < 0 || cin.fail()  )	
+
+	void Student::setName()
 	{
-	  cin.clear();
-      cin.sync();
-	 cout << "Wrong number, type it again" << endl;
-	 setNumber();
+		 cout << "enter name : " << endl;
+		 cin >> name;
 	}
-}
-void Student::setCourse()
-{
- cout << "enter course : " << endl;
- cin >> course;
- while (age < 0 || cin.fail()  )	
+
+	void Student::setGroup()
 	{
-	  cin.clear();
-      cin.sync();
-	 cout << "Wrong course, type it again" << endl;
-	 setCourse();
+		 cout << "enter group : " << endl;
+		 cin >> group;
+		 if (group <= 0)	
+		 {
+			 cout << "Wrong group, type it again" << endl;
+			 setGroup();
+		 }
 	}
-}
 
-bool Student::Datacheck()
-{
- char a[10];
- int day, month, year;
- a[0] = data[0]; //day check
- a[1] = data[1];
- day = atoi(a);
- cout << day << endl;
- if (day >= 31 || day < 1) return false;
-
- a[0] = data[3]; //month check
- a[1] = data[4];
- month = atoi(a);
- cout << month << endl;
- if (month >= 12 || month < 1) return false;
-
- a[0] = data[6]; //year check
- a[1] = data[7];
- a[2] = data[8];
- a[3] = data[9];
- year = atoi(a);
- cout << year << endl;
- if (year > 20000 || year < 1 || data[10]) return false;
-
- if (data[2,5] != '.') return false;
-
- int day2 = 8, month2 = 9, year2 = 2016; // How many years (from data xx-xx-xxxx format)
- int years = year2 - year; 
- if (years>0)
- if (month2<month || (month2 == month && day2<day)) years --;
-  cout << years << " years" <<endl;
-  age = years;
-
- return true;
-}
-
-
-void Student::setData()
-{
-	cout << "Enter birth date : " << endl;
-	cin >> data;	
-	while ((Datacheck() == false) || cin.fail()  )	
+	void Student::setSurname()
 	{
-	  cin.clear();
-      cin.sync();
-	 cout << "Wrong data, type it again" << endl;
-	 setData();	
+		 cout << "enter surname : " << endl;
+		 cin >> surname;
 	}
-}
 
-void Student::setAll()
-{
-	 setData();
-	 //setAge();
-	 setName();
-	 setSurname();
-	 setPatronymic();
-	 setId();
-	 setAdress();
-	 setNumber();
-	 setFaculty();
-	 setCourse();
-	 setGroup();
-}
+	void Student::setPatronymic()
+	{
+		 cout << "enter patronymic : " << endl;
+		 cin >> patronymic;
+	}
 
-Student::~Student(void)
-{
-}
+	void Student::setId()
+	{
+		 cout << "enter id : " << endl;
+		 cin >> id;
+		 while (id < 0 || cin.fail())	
+		 {
+			 cin.clear();
+			 cin.sync();
+			 cout << "Wrong id, type it again" << endl;
+			 setId();
+		 }
+	}
 
-void Student::print()
-{
- cout << "\n\nName - " << Student::name << endl;
- cout << "Age - " << Student::age << endl;
- cout << "Surname - " << Student::surname << endl;
- cout << "Patronic - " << Student::patronymic << endl;
- cout << "ID - " << Student::id << endl;
- cout << "Adress - " << Student::adress << endl;
- cout << "Number - " << Student::number << endl;
- cout << "Faculty - " << Student::faculty << endl;
- cout << "Course - " << Student::course << endl;
- cout << "Group - " << Student::group << endl;
-}
+	void Student::setFaculty()
+	{
+		 cout << "enter faculty : " << endl;
+		 cin >> faculty;
+	}
+
+	void Student::setAdress()
+	{
+		 cout << "enter adress : " << endl;
+		 cin >> adress;
+	}
+
+	void Student::setNumber()
+	{
+		 cout << "enter number : " << endl;
+		 cin >> number;
+		 while (age < 0 || cin.fail()  )	
+		 {
+			 cin.clear();
+			 cin.sync();
+			 cout << "Wrong number, type it again" << endl;
+			 setNumber();
+		 }
+	}
+
+	void Student::setCourse()
+	{
+		 cout << "enter course : " << endl;
+		 cin >> course;
+		 while (age < 0 || cin.fail()  )	
+		 {
+			 cin.clear();
+			 cin.sync();
+			 cout << "Wrong course, type it again" << endl;
+			 setCourse();
+		 }
+	}
+
+	bool Student::Datacheck() // Magic function 
+	{
+	 char a[10];
+	 int day, month, year;
+	 //day check
+	 a[0] = data[0]; 
+	 a[1] = data[1];
+	 day = atoi(a);
+	 cout << day << endl;
+
+	 if (day >= 31 || day < 1)
+	 {
+		return 0;
+	 }
+
+	 //month check
+	 a[0] = data[3]; 
+	 a[1] = data[4];
+	 month = atoi(a);
+	 cout << month << endl;
+
+	 if (month >= 12 || month < 1)
+	 {
+		 return 0;
+	 }
+
+	 //year check
+	 a[0] = data[6]; 
+	 a[1] = data[7];
+	 a[2] = data[8];
+	 a[3] = data[9];
+	 year = atoi(a);
+	 cout << year << endl;
+
+	 if (year > 20000 || year < 1 || data[10])
+	 {
+		return 0;
+	 }
+
+	 if (data[2,5] != '.')
+	 {
+		return 0;
+	 }
+
+	 // How many years (from data xx-xx-xxxx format)
+	 int day2 = 8, month2 = 9, year2 = 2016; 
+	 int years = year2 - year; 
+	 if (years>0)
+	 if (month2<month || (month2 == month && day2<day)) years --;
+	 cout << years << " years" <<endl;
+	 age = years;
+
+	 return 1;
+	}
+
+	void Student::setData()
+	{
+		cout << "Enter birth date : " << endl;
+		cin >> data;	
+		while ( !Datacheck() || cin.fail() )	
+		{
+			cin.clear();
+			cin.sync();
+			cout << "Wrong data, type it again" << endl;
+			setData();	
+		}
+	}
+
+	void Student::setAll()
+	{
+		 setData();
+		 setName();
+		 setSurname();
+		 setPatronymic();
+		 setId();
+		 setAdress();
+		 setNumber();
+		 setFaculty();
+		 setCourse();
+		 setGroup();
+	}
 
 
+	Student::~Student(void)
+	{
+	}
+
+	void Student::print()
+	{
+		 cout << "\n\nName - " << Student::name << endl;
+		 cout << "Age - " << Student::age << endl;
+		 cout << "Surname - " << Student::surname << endl;
+		 cout << "Patronic - " << Student::patronymic << endl;
+		 cout << "ID - " << Student::id << endl;
+		 cout << "Adress - " << Student::adress << endl;
+		 cout << "Number - " << Student::number << endl;
+		 cout << "Faculty - " << Student::faculty << endl;
+		 cout << "Course - " << Student::course << endl;
+		 cout << "Group - " << Student::group << endl;
+	}
 
 
-char* Student::getName(void)
-{
- return name;
-}
-int Student::getAge(void)
-{
- return age;
-}
-char* Student::getSurname(void)
-{
- return surname;
-}
-char* Student::getSatronymic(void)
-{
- return patronymic;
-}
-int Student::getId(void)
-{
- return id;
-}
-char* Student::getAdress(void)
-{
- return adress;
-}
-int Student::getNumber(void)
-{
- return number;
-}
-char* Student::getFaculty(void)
-{
- return faculty;
-}
-int Student::getCourse(void)
-{
- return course;
-}
-int Student::getGroup(void)
-{
- return group;
-}
+	char* Student::getFaculty(void)
+	{
+		return faculty;
+	}
+
+	int Student::getGroup(void)
+	{
+		return group;
+	}
--- Laba 2/Main.cpp
+++ Laba 2/Main.cpp
@@ -6,9 +6,8 @@
 Создать массив объектов. Вывести:
 a)  список студентов заданного факультета;
 b)  список учебной группы.
-
-
 */
+
 #include "Bomb.h"
 #include <iostream>
 #include <windows.h>
@@ -16,47 +15,37 @@ using namespace std;
 
 void main()
 {
- /*Student a1, a2;
- Student a3("Valik",18);//с помощью конструктора с параметрами
- a1.setAll();
- char* q = a1.getName();
- int w = a1.getAge();
- a1.print();*/
+	 Student a[2];
+	 char faculty[30]; 
+	 int group ;
 
- Student a[2];
- char fakultet[30];
- cout << "Enter the faculty to compare with other: " << endl;
- cin >> fakultet;
- for (int i = 0 ; i < 2 ; i ++)
- {
-	 cout << "--------CREATING A NEW STUDENT--------" << endl;
-	 a[i].setAll();
- }
+	 cout << "Enter the faculty to compare with other: " << endl;
+	 cin >> faculty;
 
- cout <<"\n\n\n"<< fakultet << "students" << endl;
-  for (int i = 0 ; i < 2 ; i ++)
- {
-	 if ( !strcmp(a[i].getFaculty(),fakultet) )
+	 for (int i = 0 ; i < 2 ; i ++)
 	 {
-		 a[i].print();
+				cout << "--------CREATING A NEW STUDENT--------" << endl;
+				a[i].setAll();
 	 }
 
- }
+	  cout <<"\n\n\n"<< faculty << "students" << endl;
+	  for (int i = 0 ; i < 2 ; i ++)
+	  {
+				if ( !strcmp(a[i].getFaculty(),faculty))
+				{
+						a[i].print();
+   				}
+	  }
 
-  int group ;
-  cout << "Enter the group number and you'll see a student" << endl;
-  cin >> group;
-  for (int i = 0 ; i < 2 ; i ++)
- {
-	 if ( a[i].getGroup() == group) 
-	 {
-		 a[i].print();
-	 }
+	  cout << "Enter the group number and you'll see a student" << endl;
+	  cin >> group;
+	  for (int i = 0 ; i < 2 ; i ++)
+	  {
+				if ( a[i].getGroup() == group) 
+				{
+						a[i].print();
+				}
+	  }
 
- }
-
-
- 	
-
- system("pause");
+	  system("pause");
 }
